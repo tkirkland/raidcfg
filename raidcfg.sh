2444ce2180b620a452700c3448021cede9bbcc86
Tony <tony@tonykirkland.net>
2024-03-31 09:55:46 -0400

    finished drive selection

--- raidcfg.sh
+++ raidcfg.sh
@@ -8,33 +8,33 @@
 # get os ID {neon, debian etc)
 function get_os_id() {
   if [[ ${ID} != "neon" && ${ID} != "ubuntu" && ${ID} != "debian" ]]; then
-      error_exit "${err}${ID} OS found and is currently unsupported" 1
+      error_exit "${err} '${ID}' OS found and is currently unsupported" 1
   fi
-  msg "${info} detected OS ${ID} so let us proceed..."
+  msg "${ok} Detected OS '${ID}' so let us proceed..."
 }
 
-# takes two parms: string to display and int as exit code
+# takes two params: string to display and int as exit code
 function error_exit() {
-  printf "%s" "${1}"
+  printf "%b\n" "${1}"
   exit "${2}"
 }
 
 function msg() {
-  printf "%s" "${1}"
+  printf "%b\n" "${1}"
 }
 
 init_vars() {
   # set color codes for text
-  local color_red='\033[31m'
-  local color_green='\033[32m'
-  local color_reset='\033[0m'
-  local color_yellow='\033[33m'
+  red='\033[0;31m'
+  green='\033[0;32m'
+  nc='\033[0m'
+  yellow='\033[0;33m'
 
   info="[i]"
-  err="[${color_red}!${color_reset}]"
-  ok="[${color_green}+${color_reset}]"
-  warn="[${color_yellow}W${color_reset}]"
-  _="[-]"
+  err="[${red}!${nc}]"
+  ok="[${green}+${nc}]"
+  warn="[${yellow}W${nc}]"
+  sp="[ ]"
 
  if ! [ -f /etc/os-release ]; then
     error_exit "${err} unable to detect OS, '/etc/os-release' not found" 1
@@ -54,13 +54,46 @@ function check_packages() {
   done
 }
 
+scan_drives() {
+  msg "${info} Scanning attached HDD devices..."
+  mapfile -t drives < <(lsblk -d -o name | tail -n +2 | sort)
+  msg "${info} Total drives detected: ${#drives[@]}"
+}
+
 # main thread
 function main() {
+  if [[ $UID != 0 ]]; then
+    error_exit "script needs root" 1
+  fi
   init_vars
-  msg "$info raidConfig script, v0.4"
-  msg "${_}"
-  msg "$info raidConfig script, v0.4"
+  msg "$info Mdadm raid config script, v0.4"
+  msg "${sp}"
+  msg "${sp} Checking OS compatibility..."
+  get_os_id
+  msg "${sp}"
+  scan_drives
+  msg "${sp}"
+  local line
+  for ((i=0; i<${#drives[@]}; i++)); do
+    printf -v line "${green}%b${nc}) %s " $((i + 1)) "${drives[i]}"
+    local prompt+="$line"
+  done
+  msg "[?] Select drives to include in the array (enter: n n n): ${drive_selection}"
 
+  for i in "${drive_selection[@]}"; do
+    # is char[idx] a valid int?
+    if ! [[ $i =~ ^[0-9]+$ ]]; then
+      echo "Error: $idx is not a number"
+      exit 1
+    fi
+
+    # Check if idx is within the valid range
+    if (( i < 1 || idx > ${#drives[@]} )); then
+        echo "Error: $idx is not a valid selection"
+        exit 1
+    fi
+
+  done
 }
 
 main "$@"
